6952462505c8b28992de698ef2dedd30072c3e6a
Abdullah Harits <harits.abdullah19@gmail.com>
2023-03-09 17:25:50 +0700

    add todo list

--- App.tsx
+++ App.tsx
@@ -5,9 +5,8 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import CoverScreen from "./screens/cover/CoverScreen";
-import BrideView from "./screens/main/components/BrideView";
-import CountingDayView from "./screens/main/components/CountingDayView";
+
+// TODO fix issue root url
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
